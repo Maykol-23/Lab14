5676dc9b7ff9ea0c98b78688f98754e494188828
Maykol <maykol.litari@tecsup.edu.pe>
2025-11-17 19:34:49 -0500

    "Versión inicial con código defectuoso para laboratorio"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
-package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        UserRegistrationService service = new UserRegistrationService();
+
+        // Casos de prueba simples
+        service.registerUser("juan", "123", "juan@correo"); // Contraseña muy corta
+        System.out.println(service.lastErrorMessage);
+
+        service.registerUser(null, "12345678", "correo-sin-arroba"); // username null
+        System.out.println(service.lastErrorMessage);
+
+        service.registerUser("error", "12345678", "error@correo.com"); // fuerza excepción
+        System.out.println(service.lastErrorMessage);
     }
 }
